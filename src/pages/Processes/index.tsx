6016fd22a9d7e490c2fc2cc60ea6f7695845af83
Mihai <mihai807@mail.ru>
2024-05-12 14:14:42 +0300

    search

--- src/pages/Processes/index.tsx
+++ src/pages/Processes/index.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@mui/material';
+import { Button, TextField } from '@mui/material';
 import Header from '../../components/Header';
 import Process from '../../components/Process';
 import { useEffect, useState } from 'react';
@@ -10,12 +10,18 @@ import { PProcessPost } from '../../redux/Process/types';
 
 const Processes = () => {
   const { processes, isLoading } = useSelector((state: RootState) => state.Process);
-
+  const [searchValues, setSearchValues] = useState('');
   const dispatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
     dispatch(get());
   }, []);
+
+  const onChange = (event: any) => {
+    setSearchValues(event.target.value);
+  };
+
+  console.log(searchValues);
 
   const handleDelete = (id: string) => {
     dispatch(remove(id));
@@ -30,16 +36,23 @@ const Processes = () => {
     dispatch(post(payload));
   };
 
-  const handleEdit = (id: String) => {
-    console.log(id);
-  };
-
   return (
     <div className="processes">
       <Header />
-      {processes.map((process) => (
-        <Process onDelete={handleDelete} process={process} key={process.id} />
-      ))}
+      <TextField className="processes__input" onChange={onChange} value={searchValues} label="Поиск" variant="outlined" />
+
+      {processes
+        .filter((process) => {
+          const lowSearchValues = searchValues.toLowerCase();
+
+          const title = process.title.toLowerCase();
+          const description = process.description.toLowerCase();
+
+          return title.includes(lowSearchValues) || description.includes(lowSearchValues);
+        })
+        .map((process) => (
+          <Process onDelete={handleDelete} process={process} key={process.id} />
+        ))}
       <Button onClick={handleAdd} className="processes__add-button" variant="contained">
         Добавить
       </Button>
